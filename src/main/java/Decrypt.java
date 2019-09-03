7908d037d2f90e5980aca72a3b1716a416faaed3
WendyOmollo <omollow280@gmail.com>
2019-09-03 19:11:40 +0300

    Add an encapsulation in the decrypt class

--- src/main/java/Decrypt.java
+++ src/main/java/Decrypt.java
@@ -1,2 +1,11 @@
-public class Decrypt {
+class Decrypt {
+    private String response;
+    private int key;
+    private String decrypted = "";
+
+    public Decrypt(String response , int key){
+        this.key = key;
+        this.response = response;
+        
+    }
 }
