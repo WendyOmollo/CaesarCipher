d5226a45dd5d3a93c9823a856d985452f43de907
WendyOmollo <omollow280@gmail.com>
2019-09-03 19:30:48 +0300

    Create a test for the decryption

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
+
 public class App {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Encrypt or decrypt?");
+        String choice = input.next();
+
+    }
 }
--- src/test/java/DecryptTest.java
+++ src/test/java/DecryptTest.java
@@ -1,2 +1,10 @@
-class DecryptTest{
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class DecryptTest{
+    @Test
+    public void caesarCode_testDecryption_String(){
+        Decrypt testDecryption = new Decrypt("lipps",4);
+        assertEquals("hello",testDecryption.setDecrypted());
+    }
 }
