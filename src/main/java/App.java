ca197bff92ed852e918d320edb19a87733ffbb72
WendyOmollo <omollow280@gmail.com>
2019-09-03 19:35:51 +0300

    Add a if statement to validate the input of encryption

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,5 +6,15 @@ public class App {
         System.out.println("Encrypt or decrypt?");
         String choice = input.next();
 
+        if (choice.equals("Encrypt")){
+            System.out.println("Enter the text to decrypt");
+            String response = input.next();
+            System.out.println("Enter the key");
+            int key = input.nextInt();
+            Encrypt  testEncryption = new Encrypt(response,key);
+            testEncryption.setEncrypted();
+            System.out.println(testEncryption.setEncrypted());
+
+        }
     }
 }
