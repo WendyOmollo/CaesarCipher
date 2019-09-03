c3cb9c407ce6ee34f1ee673543a29742a3dceb0c
WendyOmollo <omollow280@gmail.com>
2019-09-03 19:33:57 +0300

    Add a test for the encrypt file

--- src/test/java/EncryptTest.java
+++ src/test/java/EncryptTest.java
@@ -1,3 +1,10 @@
-class EncryptTest{
+import org.junit.*;
+import static org.junit.Assert.*;
 
+public class EncryptTest{
+    @Test
+    public void caesarCode_testEncryption_String(){
+        Encrypt testEncryption = new Encrypt("hello",4);
+        assertEquals("lipps",testEncryption.setEncrypted());
+    }
 }
